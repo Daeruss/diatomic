954a8d6acf132d9b4827594610e9fe664a488cbb
Maxim Maltsev <79099377601@yandex.ru>
2018-09-04 18:16:39 +0300

    first words

--- approximation/src/main.rs
+++ approximation/src/main.rs
@@ -1,3 +1,23 @@
+use std::fs::File;
+use std::io::Read;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut file_name = "N2.txt";
+    let mut file = File::open(file_name).expect("Ошибка открытия файла");
+    let mut file_data_string = String::new();
+//    file.read_line(&mut file_data_string).expect("Ошибка считывания первой строки")
+    file.read_to_string(&mut file_data_string).expect("Ошибка считывания файла");
+
+    let mut split_file = file_data_string.split("\n").collect::<Vec<&str>>();
+    let molecule_name_string = String::from(split_file[0]);
+    let mut split_line = molecule_name_string.split("\t").collect::<Vec<&str>>();
+    let molecule_name = split_line[0];
+    println!("{}",split_file.len());
+    println!("{}", number_of_points);
+
+
+
+//    println!("{}", file_data_string);
+//    println!("Hello, world!");
 }
